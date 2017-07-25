60b9ace4fb0bdff3fb09d8e89577252a429b8ab9
marcanpilami <marsu_pilami@msn.com>
2017-07-25 14:38:06 +0200

    Added move photo line with preference.

--- scanner/app/src/main/java/com/enioka/scanner/camera/ZbarScanViewOverlay.java
+++ scanner/app/src/main/java/com/enioka/scanner/camera/ZbarScanViewOverlay.java
@@ -1,7 +1,11 @@
 package com.enioka.scanner.camera;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -11,9 +15,12 @@ import android.view.View;
  */
 class ZbarScanViewOverlay extends View {
     public ZbarScanView dad;
+    float dragStartX = 0, dragStartY = 0;
+    private Context ctx;
 
     public ZbarScanViewOverlay(Context context, ZbarScanView dad) {
         super(context);
+        setFocusable(true);
         this.dad = dad;
     }
 
@@ -23,11 +30,99 @@ class ZbarScanViewOverlay extends View {
         if (changed) {
             dad.initRect();
         }
+
+        Activity a = this.getActivity();
+        if (a == null) {
+            return;
+        }
+
+        SharedPreferences p = a.getPreferences(Context.MODE_PRIVATE);
+        float y = p.getFloat("y", dad.y1);
+        if (y != dad.y1 && y > 0 && y < this.getHeight()) {
+            float dy = dad.y1 - y;
+            dad.y1 -= dy;
+            dad.y2 -= dy;
+            dad.y3 -= dy;
+            dad.y4 -= dy;
+            invalidate();
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         dad.drawHud(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (dragStartX == 0 && !(event.getX() > dad.x1 - 50 && event.getX() < dad.x2 + 50 && event.getY() > dad.y1 - 50 && event.getY() < dad.y3 + 50)) {
+            // We only care about touch events inside the rectangle.
+            return super.onTouchEvent(event);
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                dragStartX = event.getX();
+                dragStartY = event.getY();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                // final float dx = event.getX() - dragStartX;
+                final float dy = event.getY() - dragStartY;
+
+                dragStartX = event.getX();
+                dragStartY = event.getY();
+
+                /*dad.x1 += dx;
+                dad.x2 += dx;
+                dad.x3 += dx;
+                dad.x4 += dx;*/
+
+                if (dad.y1 + dy > 0 && dad.y1 + dy < this.getHeight()) {
+                    dad.y1 += dy;
+                    dad.y2 += dy;
+                    dad.y3 += dy;
+                    dad.y4 += dy;
+                }
+
+                invalidate();
+                return true;
+
+            case MotionEvent.ACTION_UP:
+                dragStartY = 0;
+                dragStartX = 0;
+                return super.onTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        storePreferences(dad.y1);
+    }
+
+    private void storePreferences(float y) {
+        Activity a = this.getActivity();
+        if (a == null) {
+            return;
+        }
+
+        SharedPreferences p = a.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor e = p.edit();
+        e.putFloat("y", y);
+        e.commit();
+    }
+
+    private Activity getActivity() {
+        Context context = getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
     }
 }

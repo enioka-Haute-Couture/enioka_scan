8af7bdf82a269383456791bf7a3f2d91038d7291
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2019-10-19 11:46:37 +0200

    Removed traces from slip scan

--- demoscannerapp/src/main/java/com/enioka/scanner/demo/WelcomeActivity.java
+++ demoscannerapp/src/main/java/com/enioka/scanner/demo/WelcomeActivity.java
@@ -1,21 +1,11 @@
 package com.enioka.scanner.demo;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
-
-import com.enioka.scanner.demo.slip.SlipScanActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
-    protected final static int PERMISSION_REQUEST_ID_CAMERA_SLIP_SCAN = 1790;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +17,6 @@ public class WelcomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onClickBt2(View v) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_ID_CAMERA_SLIP_SCAN);
-        } else {
-            openSlipScan();
-        }
-    }
-
     public void onClickBt3(View v) {
         Intent intent = new Intent(this, Scan2Activity.class);
         startActivity(intent);
@@ -43,25 +25,5 @@ public class WelcomeActivity extends AppCompatActivity {
     public void onClickBt4(View v) {
         Intent intent = new Intent(this, ScannerTesterActivity.class);
         startActivity(intent);
-    }
-
-    private void openSlipScan() {
-        Intent intent = new Intent(this, SlipScanActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_ID_CAMERA_SLIP_SCAN: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openSlipScan();
-                } else {
-                    Toast.makeText(this, com.enioka.scanner.R.string.scanner_status_no_camera, Toast.LENGTH_SHORT).show();
-                }
-                break;
-            }
-        }
     }
 }

81c6dc9ae18fb7173faf750d5b5972bdaa0df8e6
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2020-09-16 19:10:07 +0200

    Added picklist mode settings to zebra provider

---
+++ enioka_scan/src/main/java/com/enioka/scanner/sdk/zebraoss/commands/SetPickListMode.java
@@ -0,0 +1,13 @@
+package com.enioka.scanner.sdk.zebraoss.commands;
+
+/**
+ * Set picklist.
+ */
+public class SetPickListMode extends ParamSend {
+    public SetPickListMode(byte mode) {
+        super(402, mode);
+        if (mode < 0 || mode > 2) {
+            throw new IllegalArgumentException("picklist mode can only be 0/1/2");
+        }
+    }
+}

2736213d4754b01ee6ba0d6605d9b62e565713fc
Antoine Gonzalez <perso.antoine.gonzalez@gmail.com>
2022-05-05 15:09:59 +0200

    Update command parameters

--- enioka_scan/src/main/java/com/enioka/scanner/sdk/athesi/RD50TE/AthesiE5LScanner.java
+++ enioka_scan/src/main/java/com/enioka/scanner/sdk/athesi/RD50TE/AthesiE5LScanner.java
@@ -2,8 +2,10 @@ package com.enioka.scanner.sdk.athesi.RD50TE;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 
 import com.enioka.scanner.api.Scanner;
+import com.enioka.scanner.api.proxies.ScannerCommandCallbackProxy;
 import com.enioka.scanner.data.Barcode;
 import com.enioka.scanner.data.BarcodeType;
 import com.enioka.scanner.helpers.intent.IntentScanner;
@@ -49,12 +51,18 @@ public class AthesiE5LScanner extends IntentScanner<String> implements Scanner.W
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
-    public void pressScanTrigger() {
+    public void pressScanTrigger(@Nullable ScannerCommandCallbackProxy cb) {
         broadcastIntent(AthesiE5LIntents.PRESS_TRIGGER);
+        if (cb != null) {
+            cb.onSuccess();
+        }
     }
 
     @Override
-    public void releaseScanTrigger() {
+    public void releaseScanTrigger(@Nullable ScannerCommandCallbackProxy cb) {
         broadcastIntent(AthesiE5LIntents.RELEASE_TRIGGER);
+        if (cb != null) {
+            cb.onSuccess();
+        }
     }
 }

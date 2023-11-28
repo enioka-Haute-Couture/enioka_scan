bb6a555cc9128686ffd9dcd3f40e0830470c4234
antoine.gonzalez <antoine.gonzalez@enioka.com>
2023-11-28 15:36:14 +0100

    Add base for RTD documentation #130

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+source_encoding = 'utf-8'
+master_doc = 'index'
+
+extensions = ['myst_parser']
+myst_enable_extensions = ['colon_fence', 'strikethrough']
+
+project = 'enioka Scan'
+copyright = '2012-2020, enioka Haute Couture'
+#version = '2.4'
+#release = '2.4.1'
+hightlight_language = 'java'
+
+html_theme = 'sphinx_rtd_theme'

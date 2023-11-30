e6176445a38b5ddf874bf6b4b586470500d866ba
Thomas Cordival <tuxella@users.noreply.github.com>
2023-11-30 15:47:42 +0100

    Fix copyright dates in docs (#145)

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ extensions = ['myst_parser']
 myst_enable_extensions = ['colon_fence', 'strikethrough']
 
 project = 'enioka Scan'
-copyright = '2012-2020, enioka Haute Couture'
+copyright = '2017-2023, enioka Haute Couture'
 #version = '2.4'
 #release = '2.4.1'
 hightlight_language = 'java'
